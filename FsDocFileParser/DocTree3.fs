d31098386399477e755a5b60a8b40394487b5fbb
Jonathan Markland <Jonathan_Markland@waters.com>
2020-04-03 21:40:21 +0100

    Titles and HTML escaping

--- FsDocFileParser/DocTree3.fs
+++ FsDocFileParser/DocTree3.fs
@@ -39,7 +39,14 @@ let rec DocTree2ToDocTree3 treeList2 =
 
 
 
-let EscapedForHtml s = s
+let EscapedForHtml (s:string) = 
+
+    let Repl (searchChar:string) (replaceStr:string) (str:string) =
+        // Ensure we create no garbage when there is nothing to replace:
+        if str.Contains(searchChar) then str.Replace(searchChar, replaceStr) else str
+
+    s |> Repl "&" "&amp;" |> Repl "<" "&lt;" |> Repl ">" "&gt;" |> Repl "\"" "&quot;"
+
 
 
 
@@ -53,6 +60,15 @@ let IsStringAll chr str =
 let IsHeading1Underline = IsStringAll '='
 let IsHeading2Underline = IsStringAll '-'
 
+let IsPunctuation ch = (ch = '.') || (ch = '!') || (ch = '?') || (ch = ':')
+
+
+
+let LooksLikeSingleLineTitle (str:string) =
+    str.Length > 1
+        && str.[0] |> System.Char.IsLetterOrDigit
+        && not (str.[str.Length-1] |> IsPunctuation)
+
 
 
 let TitleWithUnderline pred treeList3 =
@@ -63,61 +79,72 @@ let TitleWithUnderline pred treeList3 =
             if pred underline then Some(title,tail) else None
         | _ -> None
 
-    
-
-
 let (|Title1WithUnderline|_|) = TitleWithUnderline IsHeading1Underline
 let (|Title2WithUnderline|_|) = TitleWithUnderline IsHeading2Underline
 
-
-
-let rec DocTree3ToHtml treeList3 =
-
-    let translated = DocTree3ToHtml
-
+let (|SingleLineTitle|_|) treeList3 =
     match treeList3 with
+        | DT3Content(title)::DT3EmptyLine::tail ->
+            if title |> LooksLikeSingleLineTitle then Some(title,tail) else None
+        | _ -> None
+
+
+let rec DocTree3ToHtmlRec nestLevel treeList3 =
+
+    let translated = DocTree3ToHtmlRec nestLevel
+    let nested = DocTree3ToHtmlRec (nestLevel + 1)
+
+    match treeList3, nestLevel with
 
         //
         // Priority patterns:
         //
 
-        | Title1WithUnderline (title,tail) ->
+        | Title1WithUnderline (title,tail), 0 ->
             printfn "<H1>%s</H1>" (title |> EscapedForHtml)
             translated tail
 
-        | Title2WithUnderline (title,tail) ->
+        | Title2WithUnderline (title,tail), 0 ->
             printfn "<H2>%s</H2>" (title |> EscapedForHtml)
+            translated tail
+
+        | SingleLineTitle (title,tail), 0 ->
+            printfn "<H3>%s</H3>" (title |> EscapedForHtml)
             translated tail
 
         //
         // Basic fallbacks:
         //
 
-        | DT3EmptyLine::tail ->
+        | DT3EmptyLine::tail, _ ->
             printfn "<br/>"
             translated tail
 
-        | DT3Content(str)::tail ->
+        | DT3Content(str)::tail, _ ->
             printfn "%s<br/>" (str |> EscapedForHtml)
             translated tail
 
-        | DT3Indent(lst)::tail ->
+        | DT3Indent(lst)::tail, _ ->
             printfn "<blockquote>"
-            translated lst
+            nested lst
             printfn "</blockquote>"
             translated tail
 
-        | DT3Bullet(lst)::tail ->
+        | DT3Bullet(lst)::tail, _ ->
             printfn "<li>"
-            translated lst
+            nested lst
             printfn "</li>"
             translated tail
 
-        | DT3PageBreak::tail ->
+        | DT3PageBreak::tail, _ ->
             printfn "<hr/>"
             translated tail
 
-        | [] ->
+        | [], _ ->
             ()
+
+
+
+let DocTree3ToHtml = DocTree3ToHtmlRec 0
 
 
