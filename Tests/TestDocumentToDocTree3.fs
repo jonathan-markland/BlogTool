8fff40a7c266b613e87b49ed20b539e33d45f548
Jonathan Markland <Jonathan_Markland@waters.com>
2020-04-12 11:38:46 +0100

    Tests

--- Tests/TestDocumentToDocTree3.fs
+++ Tests/TestDocumentToDocTree3.fs
@@ -137,19 +137,141 @@ let ``Heading 3 single line title`` () =
         |> Is [ DT3Heading(Heading3, "Examples") ]
 
 // ----------------------------------------------------------------------------------------------
-//  1:1 stuff
+//  Page breaks
 // ----------------------------------------------------------------------------------------------
 
-// oops move to DocTree2 to DocTree3 test file  [<Fact>]
-// oops move to DocTree2 to DocTree3 test file  let ``Empty lines discarded`` () = 
-// oops move to DocTree2 to DocTree3 test file      [| "" |] |> Is []
-// oops move to DocTree2 to DocTree3 test file  
-// oops move to DocTree2 to DocTree3 test file  [<Fact>]
-// oops move to DocTree2 to DocTree3 test file  let ``Content becomes Paragraph`` () = 
-// oops move to DocTree2 to DocTree3 test file      [| "content" |] |> Is [ DT3Paragraph "content" ]
-        
-//        | DT2Content(str)::tail      ->  DT3Paragraph(str)::(translated tail)
-//        | DT2Indent(lst)::tail       ->  DT3Indent(translated lst)::(translated tail)
-//        | DT2Preformatted(x)::tail   ->  DT3Preformatted(x)::(translated tail)
-//        | DT2Bullet(lst)::tail       ->  DT3Bullet(translated lst)::(translated tail)
-//        | DT2PageBreak::tail         ->  DT3PageBreak::(translated tail)
+[<Fact>]
+let ``Page breaks`` () = 
+    [| 
+        "An example."
+        "    Indented paragraph"
+        "    of two lines."
+        ""   // Reminder:  Multiple blank lines do NOT combine to page breaks in an indented context.
+        ""
+        "    Followed by a second paragraph."
+        "And back."
+        ""
+        ""
+        "On next page."
+    |] 
+        |> Is 
+            [
+                DT3Paragraph("An example.")
+                DT3Indent([
+                    DT3Paragraph("Indented paragraph of two lines.")
+                    DT3Paragraph("Followed by a second paragraph.")
+                ])
+                DT3Paragraph("And back.")
+                DT3PageBreak
+                DT3Paragraph("On next page.")
+            ]
+
+// ----------------------------------------------------------------------------------------------
+//  Preformatted
+// ----------------------------------------------------------------------------------------------
+
+[<Fact>]
+let ``Preformatted`` () = 
+    [| 
+        "If-expressions"
+        ""
+        "An example of if-expressions"
+        "in the F# programming language."
+        ""
+        "F#:"
+        ""
+        "    if x > y then a else b"
+        ""
+        "Next paragraph."
+    |] 
+        |> Is 
+            [
+                DT3Heading(Heading3, "If-expressions")
+                DT3Paragraph "An example of if-expressions in the F# programming language."
+                DT3Paragraph "F#:"
+                DT3Preformatted([
+                    PreformattedString "if x > y then a else b"
+                ])
+                DT3Paragraph "Next paragraph."
+            ]
+
+[<Fact>]
+let ``Preformatted 2`` () = 
+    [| 
+        "Match-expressions"
+        ""
+        "An example of match-expressions"
+        "in the F# programming language."
+        ""
+        "F#:"
+        ""
+        "    let myList = if something then list1 else list2"
+        ""
+        "    match myList with"
+        "        | head::tail -> (processed head)::(translated tail)"
+        "        | [] -> []"
+        ""
+        "Next paragraph."
+    |] 
+        |> Is 
+            [
+                DT3Heading(Heading3, "Match-expressions")
+                DT3Paragraph "An example of match-expressions in the F# programming language."
+                DT3Paragraph "F#:"
+                DT3Preformatted([
+                    PreformattedString "let myList = if something then list1 else list2"
+                    PreformattedString ""
+                    PreformattedString "match myList with"
+                    PreformattedString "    | head::tail -> (processed head)::(translated tail)"
+                    PreformattedString "    | [] -> []"
+                ])
+                DT3Paragraph "Next paragraph."
+            ]
+
+// ----------------------------------------------------------------------------------------------
+//  Bullets
+// ----------------------------------------------------------------------------------------------
+
+[<Fact>]
+let ``Bullets basic`` () = 
+    [| 
+        "- First case."
+        "- Second case."
+    |] 
+        |> Is 
+            [
+                DT3Bullet ([DT3Paragraph "First case."])
+                DT3Bullet ([DT3Paragraph "Second case."])
+            ]
+
+[<Fact>]
+let ``Bullets with wrapping paragraph`` () = 
+    [| 
+        "- First case."
+        "- Second case"
+        "  which is longer."
+        "- Third case."
+    |] 
+        |> Is 
+            [
+                DT3Bullet ([DT3Paragraph "First case."])
+                DT3Bullet ([DT3Paragraph "Second case which is longer."])
+                DT3Bullet ([DT3Paragraph "Third case."])
+            ]
+
+[<Fact>]
+let ``Bullets with wrapping paragraph and blank lines`` () = 
+    [| 
+        "- First case."
+        ""
+        "- Second case"
+        "  which is longer."
+        ""
+        "- Third case."
+    |] 
+        |> Is 
+            [
+                DT3Bullet ([DT3Paragraph "First case."])
+                DT3Bullet ([DT3Paragraph "Second case which is longer."])
+                DT3Bullet ([DT3Paragraph "Third case."])
+            ]
